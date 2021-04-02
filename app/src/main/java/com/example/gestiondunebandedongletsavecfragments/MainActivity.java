c5e925353e1894373d5fd9bc27a79ecc52c1c817
marco287 <63555641+marco287@users.noreply.github.com>
2021-04-02 23:29:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gestiondunebandedongletsavecfragments/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.gestiondunebandedongletsavecfragments;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.annotation.RequiresApi;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+import com.example.gestiondunebandedongletsavecfragments.ui.main.SectionsPagerAdapter;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewpager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.settings) {
+            Snackbar.make(viewpager,"setting",Snackbar.LENGTH_LONG).show();
+            return true;
+        }
+        if (id == R.id.modeavion) {
+            Snackbar.make(viewpager, "setting", Snackbar.LENGTH_LONG).show();
+            return true;
+        }
+        if (id == R.id.modeaviondesactivé) {
+            Snackbar.make(viewpager, "setting", Snackbar.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/gestiondunebandedongletsavecfragments/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,83 @@
+package com.example.gestiondunebandedongletsavecfragments.ui.main;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.gestiondunebandedongletsavecfragments.NatureFragment;
+import com.example.gestiondunebandedongletsavecfragments.R;
+
+import java.util.Locale;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+  //  @StringRes
+    //private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                return NatureFragment.newInstance(0, mContext.getString(R.string.titre_section0));
+            case 1:
+                return NatureFragment.newInstance(1, mContext.getString(R.string.titre_section1));
+            case 2:
+                return NatureFragment.newInstance(2, mContext.getString(R.string.titre_section2));
+
+
+        }
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        Locale l = Locale.getDefault();
+        String titre = "";
+        Drawable icone = null;
+        switch (position) {
+            case 0:
+                titre = mContext.getString(R.string.titre_section0).toUpperCase(l);
+                icone = mContext.getResources().getDrawable(R.drawable.mineraux);
+                break;
+            case 1:
+                titre = mContext.getString(R.string.titre_section1).toUpperCase(l);
+                icone = mContext.getResources().getDrawable(R.drawable.vegetaux);
+                break;
+            case 2:
+                titre = mContext.getString(R.string.titre_section2).toUpperCase(l);
+                icone = mContext.getResources().getDrawable(R.drawable.animaux);
+                break;
+        }
+        SpannableString sb = new SpannableString(" " + titre);
+
+        icone.setBounds(0, 0, icone.getIntrinsicWidth(), icone.getIntrinsicHeight());
+        ImageSpan span = new ImageSpan(icone, ImageSpan.ALIGN_BASELINE);
+        sb.setSpan(span, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return sb;
+
+    }
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
